20dca1fd20df2999cf90cc0b548705302b210385
EmanuelCruz <emanuelcruz091@gmail.com>
2021-08-15 01:02:19 -0300

    test deploy

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-if (!isset($_REQUEST['controller'])) {
-    require_once 'controller/animal_controller.php';
-    $controller = new AnimalController();
-    $controller->indexAnimal();
 
-} else {
-    $controller = $_REQUEST['controller'];
-    $action = $_REQUEST['action'];
-    require_once 'controller/' . $controller . '_controller.php';
-    $controller = ucwords($controller) . 'Controller';
-    $controller = new $controller;
-    call_user_func(array($controller, $action));
-}
+echo "Hola Mundo";
+
+//if (!isset($_REQUEST['controller'])) {
+//    require_once 'controller/animal_controller.php';
+//    $controller = new AnimalController();
+//    $controller->indexAnimal();
+//
+//} else {
+//    $controller = $_REQUEST['controller'];
+//    $action = $_REQUEST['action'];
+//    require_once 'controller/' . $controller . '_controller.php';
+//    $controller = ucwords($controller) . 'Controller';
+//    $controller = new $controller;
+//    call_user_func(array($controller, $action));
+//}
