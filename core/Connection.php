3f1c870f4577a19c7629e33299a658d6faccf442
EmanuelCruz <emanuelcruz091@gmail.com>
2021-08-15 00:35:17 -0300

    feat: add environment variables for database connection

--- core/Connection.php
+++ core/Connection.php
@@ -1,24 +1,35 @@
 <?php
 
+
 class Connection
 {
-    private $driver = "mysql";
-    private $host = 'localhost'; //IP del Servidor, cuando hagamos el deploy
-    private $user = 'root'; //Puede ser distinto cuando hagamos el deploy
-    private $password = '';
-    private $dbName = 'pet_adoption';
-    private $charset = 'utf8'; //Con esto evitamos problemas con caracteres en español
+    private string $driver;
+    private string $host;
+    private string $user;
+    private string $password;
+    private string $dbName;
+    private string $charset = 'utf8';
 
     public function conexion()
     {
         try {
+            require_once 'vendor/autoload.php';
+            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+            $dotenv->load();
+
+            $this->driver = $_ENV["DB_DRIVER"];
+            $this->host = $_ENV["DB_HOST"];
+            $this->user = $_ENV["DB_USER"];
+            $this->password = $_ENV["DB_PASSWORD"];
+            $this->dbName = $_ENV["DB_NAME"];
+
             $pdo = new PDO("{$this->driver}:host={$this->host};dbname={$this->dbName};charset={$this->charset}", $this->user, $this->password);
             $pdo->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // ATTR_ERRMODE: Reporte de errores
             // ERRMODE_EXCEPTION: Lanza exceptions que las va a capturar PDOException
             return $pdo;
         } catch (PDOException $e) {
-            echo $e->getMessage();
+            return $e->getMessage();
         }
     }
 }
