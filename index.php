6590aef496e4c0ef0bbbbc2a0524044dee016cca
EmanuelCruz <emanuelcruz091@gmail.com>
2021-08-15 15:52:02 -0300

    fix: remove test code for deployment

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
-echo "Hola Mundo";
+if (!isset($_REQUEST['controller'])) {
+    require_once 'controller/animal_controller.php';
+    $controller = new AnimalController();
+    $controller->indexAnimal();
 
-//if (!isset($_REQUEST['controller'])) {
-//    require_once 'controller/animal_controller.php';
-//    $controller = new AnimalController();
-//    $controller->indexAnimal();
-//
-//} else {
-//    $controller = $_REQUEST['controller'];
-//    $action = $_REQUEST['action'];
-//    require_once 'controller/' . $controller . '_controller.php';
-//    $controller = ucwords($controller) . 'Controller';
-//    $controller = new $controller;
-//    call_user_func(array($controller, $action));
-//}
+} else {
+    $controller = $_REQUEST['controller'];
+    $action = $_REQUEST['action'];
+    require_once 'controller/' . $controller . '_controller.php';
+    $controller = ucwords($controller) . 'Controller';
+    $controller = new $controller;
+    call_user_func(array($controller, $action));
+}
